becdb0fbaf1e14008688bff55b653ba86d9d8e9d
dannyswagg <ugiomohd@yahoo.com>
2024-09-19 12:52:27 +0100

    layouts

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,32 +1,67 @@
 import React from "react";
 import pattern from "../assets/pattern-bg-desktop.png";
+import patternMobile from "../assets/pattern-bg-mobile.png";
 import icon from "../assets/icon-arrow.svg";
 
 const Home = () => {
   return (
     <>
       <div className="relative">
-        <img src={pattern} alt="pattern" className="h-full" />
+        <img
+          src={pattern}
+          alt="pattern"
+          className="h-60 w-full object-fit hidden md:block"
+        />
+        <img
+          src={patternMobile}
+          alt="mobile pattern"
+          className="w-full h-[16rem] md:hidden"
+        />
         <div className="absolute inset-x-0 top-0 mt-5 text-center">
-          <h1 className="text-white text-3xl font-bold">IP Address Tracker</h1>
-          <div className="flex items-center justify-center mt-5">
+          <h1 className="text-white text-2xl md:text-3xl font-bold">
+            IP Address Tracker
+          </h1>
+          <div className="flex items-center justify-center w-full mt-5">
             <input
-              className="w-1/3 rounded-l-lg p-3 outline-none text-black"
+              className="w-[75%] md:w-1/3 rounded-l-xl p-3 outline-none text-black placeholder:text-xs"
               type="text"
               placeholder="Search for any IP address or domain"
             />
-            <button className="cursor-pointer bg-black text-white rounded-r-lg h-[50px]">
+            <button className="cursor-pointer bg-black text-white rounded-r-xl h-[50px]">
               <img src={icon} alt="arrow" className="px-5" />
             </button>
           </div>
-          <ul className="bg-slate-100 rounded-lg mt-14 h-40 w-[70%] mx-auto p-5 flex justify-around">
-            <li>IP Address</li>
-            <li>LOCATION</li>
-            <li>TIMEZONE</li>
-            <li>ISP</li>
+          <ul
+            className="bg-slate-100 rounded-lg mt-5 md:mt-12 w-[90%] md:w-[70%] 
+          mx-auto py-7 px-5 block md:flex items-center justify-between"
+          >
+            <li className="text-center md:text-left flex-col">
+              <h2 className="font-light text-sm text-darkGrey">IP Address</h2>
+              <h2 className="font-bold text-2xl text-veryDarkGrey">
+                192.212.174.101
+              </h2>
+            </li>
+            <li className="text-center md:text-left flex-col">
+              <h2 className="font-light text-sm text-darkGrey">LOCATION</h2>
+              <h2 className="font-bold text-2xl text-veryDarkGrey">
+                Brooklyn, NY 10001
+              </h2>
+            </li>
+            <li className="text-center md:text-left flex-col">
+              <h2 className="font-light text-sm text-darkGrey">TIMEZONE</h2>
+              <h2 className="font-bold text-2xl text-veryDarkGrey">
+                UTC -05:00
+              </h2>
+            </li>
+            <li className="text-center md:text-left flex-col">
+              <h2 className="font-light text-sm text-darkGrey">ISP</h2>
+              <h2 className="font-bold text-2xl text-veryDarkGrey">
+                SpaceX Starlink
+              </h2>
+            </li>
           </ul>
         </div>
-        <div className="bg-black h-[500px] w-full">hjbjhbhjbj</div>
+        <div className="bg-black height"></div>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        veryDarkGrey: "hsl(0, 0%, 17%)",
+        darkGrey: "hsl(0, 0%, 59%)",
+      },
+    },
   },
   plugins: [],
 };
 
-// - Very Dark Gray: hsl(0, 0%, 17%)
-// - Dark Gray: hsl(0, 0%, 59%)
